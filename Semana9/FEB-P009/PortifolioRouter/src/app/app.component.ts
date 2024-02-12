87ae898afee74e425ac6825643732c3835e5b978
DStalkerBR <jhon89araujo@gmail.com>
2024-02-12 17:45:57 -0300

    setup routing by adding imports

--- Semana9/FEB-P009/PortifolioRouter/src/app/app.component.ts
+++ Semana9/FEB-P009/PortifolioRouter/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { TurismoComponent } from './turismo/turismo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, TurismoComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, TurismoComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
